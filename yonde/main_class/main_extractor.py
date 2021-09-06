67d1f5542114541f4a8480623f1601bbee430e22
yanhuishi <87889485+yanhuishi@users.noreply.github.com>
2021-09-06 20:21:52 -0300

    Método do banner implementado diretamente na classe principal

--- yonde/main_class/main_extractor.py
+++ yonde/main_class/main_extractor.py
@@ -1,6 +1,8 @@
 from yonde.downloader.downloader import Downloader
 from yonde.session.session import Session
 from yonde.printer.printer import Printer
+from yonde.__version__ import __version__
+from yonde.color.color import Color
 from lxml.html import fromstring
 import json
 import re
@@ -18,6 +20,7 @@ class MainExtractor(object):
         self._output = output
         self._capitulos = []
         self._soup = None
+        self.c = Color()
         self._url = url
         self._site_name = self.re_findall('https?://(?:www.)?.+(?:\.net|\.com(?:\.br)?|\.top|\.site)', self._url)[0]
         self._downloader = Downloader(self._session, self._output, self._threads, self._download_type)
@@ -45,10 +48,6 @@ class MainExtractor(object):
     def create_soup(self, html):
         self._soup = fromstring(html)
 
-    def banner(self, warning=None):
-        return Printer(site_name=self._site_name, total_caps=len(self._capitulos), manga_name=self._manga_name,
-                       threads=self._threads, no_banner=self._no_banner).banner(warning)
-
     def download(self, cap_atual, nome_pdf, urls):
         return self._downloader.download_manga(self._manga_name, cap_atual, nome_pdf, urls)
 
@@ -63,3 +62,56 @@ class MainExtractor(object):
 
     def runner(self):
         pass
+
+    def banner(self, warning=None):
+        if self._no_banner:
+            print("""
+ \033[95m:: \033[93mBaixando \033[94m{}
+ \033[95m:: \033[93mBaixando de \033[94m{}
+ \033[95m:: \033[93mBaixando com \033[94m{} \033[93mthreads
+ \033[95m:: \033[93mBaixando \033[94m{} \033[93mcapitulos:\033[0m
+            """.format(self._manga_name.lower().replace('_', '-'), self._site_name, self._threads,
+                       len(self._capitulos)), end='\r')
+            if warning:
+                print(' \033[91m\033[1m!! {}'.format(warning))
+        else:
+            print("""
+ \033[93m{:^44}
+ \033[93m{:^44}
+ \033[93m{:^44}
+ \033[93m{:^44}
+ \033[94m{:^44}
+ \033[94m{:^44}
+ \033[94m{:^44}
+ \033[94m{:^44}
+
+\033[0m
+ [\033[95m--\033[0m]{:^36}[\033[95m--\033[0m]
+ [\033[95m--\033[0m]{:^45}[\033[95m--\033[0m]
+ [\033[95m--\033[0m]{:^49}[\033[95m--\033[0m]
+
+
+
+ \033[95m:: \033[93mBaixando \033[94m{}
+ \033[95m:: \033[93mBaixando de \033[94m{}
+ \033[95m:: \033[93mBaixando com \033[94m{} \033[93mthreads
+ \033[95m:: \033[93mBaixando \033[94m{} \033[93mcapitulos:\033[0m
+""".format(
+                "                       _      _ ",
+                "                      | |    | |",
+                " _   _  ___  _ __   __| | ___| |",
+                "| | | |/ _ \| '_ \ / _` |/ _ \ |",
+                "| |_| | (_) | | | | (_| |  __/_|",
+                " \__, |\___/|_| |_|\__,_|\___(_)",
+                "  __/ |                         ",
+                " |___/                          ",
+                "https://github.com/yonde-manga",
+                "Versão \033[91m{}\033[0m".format(__version__),
+                "by \033[91m\033[1myanhuishi\033[0m",
+                self._manga_name.lower().replace('_', '-'),
+                self._site_name,
+                self._threads,
+                len(self._capitulos)
+            ), end='\r')
+            if warning:
+                print(' \033[91m\033[1m!! {}'.format(warning))
