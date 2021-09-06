76132fa624ba7f3edb8b23b1878a6088061e874d
yanhuishi <87889485+yanhuishi@users.noreply.github.com>
2021-09-06 20:52:31 -0300

    Alteração de cores no banner para funcionar no Windows (implementação do colorama)

--- yonde/main_class/main_extractor.py
+++ yonde/main_class/main_extractor.py
@@ -65,53 +65,54 @@ class MainExtractor(object):
 
     def banner(self, warning=None):
         if self._no_banner:
-            print("""
- \033[95m:: \033[93mBaixando \033[94m{}
- \033[95m:: \033[93mBaixando de \033[94m{}
- \033[95m:: \033[93mBaixando com \033[94m{} \033[93mthreads
- \033[95m:: \033[93mBaixando \033[94m{} \033[93mcapitulos:\033[0m
-            """.format(self._manga_name.lower().replace('_', '-'), self._site_name, self._threads,
-                       len(self._capitulos)), end='\r')
+            print(f"""
+ {self.c.magenta}:: {self.c.yellow}Baixando {self.c.blue}{self._manga_name.lower().replace('_', '-')}
+ {self.c.magenta}:: {self.c.yellow}Baixando de {self.c.blue}{self._site_name}
+ {self.c.magenta}:: {self.c.yellow}Baixando com {self.c.blue}{self._threads} {self.c.yellow}threads
+ {self.c.magenta}:: {self.c.yellow}Baixando {self.c.blue}{len(self._capitulos)} {self.c.yellow}capitulos:{self.c.reset}
+            """, end='\r')
             if warning:
-                print(' \033[91m\033[1m!! {}'.format(warning))
+                print(f' {self.c.bold}{self.c.red}!! {warning}{self.c.reset}')
         else:
             print("""
- \033[93m{:^44}
- \033[93m{:^44}
- \033[93m{:^44}
- \033[93m{:^44}
- \033[94m{:^44}
- \033[94m{:^44}
- \033[94m{:^44}
- \033[94m{:^44}
+ {}{:^44}
+ {}{:^44}
+ {}{:^44}
+ {}{:^44}
+ {}{:^44}
+ {}{:^44}
+ {}{:^44}
+ {}{:^44}
 
-\033[0m
- [\033[95m--\033[0m]{:^36}[\033[95m--\033[0m]
- [\033[95m--\033[0m]{:^45}[\033[95m--\033[0m]
- [\033[95m--\033[0m]{:^49}[\033[95m--\033[0m]
+{}
+ [{}--{}]{:^36}[{}--{}]
+ [{}--{}]{:^45}[{}--{}]
+ [{}--{}]{:^49}[{}--{}]
 
 
 
- \033[95m:: \033[93mBaixando \033[94m{}
- \033[95m:: \033[93mBaixando de \033[94m{}
- \033[95m:: \033[93mBaixando com \033[94m{} \033[93mthreads
- \033[95m:: \033[93mBaixando \033[94m{} \033[93mcapitulos:\033[0m
+ {}:: {}Baixando {}{}
+ {}:: {}Baixando de {}{}
+ {}:: {}Baixando com {}{} {}threads
+ {}:: {}Baixando {}{} {}capitulos:\033[0m
 """.format(
-                "                       _      _ ",
-                "                      | |    | |",
-                " _   _  ___  _ __   __| | ___| |",
-                "| | | |/ _ \| '_ \ / _` |/ _ \ |",
-                "| |_| | (_) | | | | (_| |  __/_|",
-                " \__, |\___/|_| |_|\__,_|\___(_)",
-                "  __/ |                         ",
-                " |___/                          ",
-                "https://github.com/yonde-manga",
-                "Versão \033[91m{}\033[0m".format(__version__),
-                "by \033[91m\033[1myanhuishi\033[0m",
-                self._manga_name.lower().replace('_', '-'),
-                self._site_name,
-                self._threads,
-                len(self._capitulos)
+                self.c.yellow, "                       _      _ ",
+                self.c.yellow, "                      | |    | |",
+                self.c.yellow, " _   _  ___  _ __   __| | ___| |",
+                self.c.yellow, "| | | |/ _ \| '_ \ / _` |/ _ \ |",
+                self.c.blue,   "| |_| | (_) | | | | (_| |  __/_|",
+                self.c.blue,   " \__, |\___/|_| |_|\__,_|\___(_)",
+                self.c.blue,   "  __/ |                         ",
+                self.c.blue,   " |___/                          ",
+                self.c.reset,
+                self.c.magenta, self.c.reset, "https://github.com/yonde-manga", self.c.magenta, self.c.reset,
+                self.c.magenta, self.c.reset, "Versão \033[91m{}\033[0m".format(__version__), self.c.magenta,
+                self.c.reset,
+                self.c.magenta, self.c.reset, "by \033[91m\033[1myanhuishi\033[0m", self.c.magenta, self.c.reset,
+                self.c.magenta, self.c.yellow, self.c.blue, self._manga_name.lower().replace('_', '-'),
+                self.c.magenta, self.c.yellow, self.c.blue, self._site_name,
+                self.c.magenta, self.c.yellow, self.c.blue, self._threads, self.c.yellow,
+                self.c.magenta, self.c.yellow, self.c.blue, len(self._capitulos), self.c.yellow, self.c.reset,
             ), end='\r')
             if warning:
-                print(' \033[91m\033[1m!! {}'.format(warning))
+                print(f' {self.c.bold}{self.c.red}!! {warning}{self.c.reset}')
